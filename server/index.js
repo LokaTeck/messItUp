e0e854eabda6944cafc515d7fe162d89f8755dca
Logan Rosenlund <loganrosenlund61@gmail.com>
2021-02-26 15:03:06 -0800

    Showing Mitch git branch workflow.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const PORT = 3001;
 
 app.use(express.static('public'));
 
+app.get('/test', (req, res) => {
+  res.send('Hello world!');
+})
+
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 })
